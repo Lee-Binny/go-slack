e5f3c62eae310ba191084269dd9712df2bf16a48
이수빈 <qlsdl9516@gmail.com>
2022-01-16 01:35:57 +0900

    front / router 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
